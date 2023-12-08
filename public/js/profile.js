cab9243ed17332646bf8eb2abbdfb3b9ab6db744
tugwellchristi <tugwellchristi@gmail.com>
2023-12-08 12:45:45 -0800

    updated json content

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,29 @@
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const usernameInput = document.getElementById('username-login');
+    const passwordInput = document.getElementById('password-login');
 
-//     const 
-// }
+    // If form exists and username and password are not empty
+    if (form && usernameInput && passwordInput) {
+        form.addEventListener('loginBtn2', function (event) {
+            const username = usernameInput.value;
+            const password = passwordInput.value;
+            let formValid = true;
+            
+            // If username is empty, show error on page
+            if (username.trim() === '') {
+                formValid = false;
+            }
+
+            // If password is empty, show error on page
+            if (password.trim() === '') {
+                formValue = false;
+            }
+
+            // If form is not valid 
+            if (!formValid) {
+                event.preventDefault();
+            }
+        });
+    }
+});
